fe9c7c99164ca542aaa0033fee4a114c9f3d91f4
Subbulakshmi Muruganandam <105669253+subbums83@users.noreply.github.com>
2022-06-03 16:24:30 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int var1{44};
+    int var2(45);
+    int var3=46;
+
+
+    cout << var1<<" , "<<var2<<" , "<<var3<< endl;
+    return 0;
+}
